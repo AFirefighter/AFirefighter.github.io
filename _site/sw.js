1dcf41a0d3c4ea8ab4aa25071396a606974ed847
ygg_real <3139527462@qq.com>
2023-10-26 15:16:44 +0800

    rfix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1698241839';
+const cacheName = 'chirpy-1698304573';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
